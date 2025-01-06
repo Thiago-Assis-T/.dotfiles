2528ad1d9468db878cd0bc7073a02319e9380023
Thiago Assis <thiago.assisfernandes@gmail.com>
2025-01-06 20:46:58 -0300

    no background

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -2,6 +2,19 @@ return {
 	"Shatur/neovim-ayu",
 	init = function()
 		-- Colorscheme set:
-		vim.cmd.colorscheme("ayu-dark")
+		require("ayu").setup({
+			overrides = {
+				Normal = { bg = "None" },
+				NormalFloat = { bg = "none" },
+				ColorColumn = { bg = "None" },
+				SignColumn = { bg = "None" },
+				Folded = { bg = "None" },
+				FoldColumn = { bg = "None" },
+				CursorLine = { bg = "None" },
+				CursorColumn = { bg = "None" },
+				VertSplit = { bg = "None" },
+			},
+		})
+		vim.cmd.colorscheme("ayu-mirage")
 	end,
 }

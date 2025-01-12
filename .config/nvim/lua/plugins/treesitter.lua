79c8650d5842121a2fe6bdf3bbc2f897fc1ccb76
Thiago Assis <thiago.assisfernandes@gmail.com>
2025-01-11 23:41:18 -0300

    python treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,19 @@
 return {
-        "nvim-treesitter/nvim-treesitter",
-        build = ':TSUpdate',
-        opts = {
-                ensure_installed = {
-                        "lua",
-                },
-                auto_install = true,
-                highlight = {
-                        enable = true,
-                        additional_vim_regex_highlighting = true,
-                },
-                indent = {
-                        enable = true,
-                },
-        },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"c",
+			"python",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+		indent = {
+			enable = true,
+		},
+	},
 }
-

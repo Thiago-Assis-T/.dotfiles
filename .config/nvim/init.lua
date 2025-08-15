1af79a0c503f192331f5687d6247719bedee0434
Thiago Assis <thiago.assisfernandes@gmail.com>
2025-08-15 09:40:45 -0300

    starting neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.swapfile = false
+vim.opt.tabstop = 3
+vim.opt.swapfile = false
+
+vim.global.mapleader = " "
+
+vim.cmd.colorscheme = "retrobox"
+
+
